7d0c3d2a60b7638c6593d679e3714ae159859567
buhtan <buhtant@gmail.com>
2022-09-25 20:00:26 +0300

    Create degiskenlle.java
    degişkenler

---
+++ degiskenlle.java
@@ -0,0 +1,16 @@
+package degişkenler;
+
+public class degiskenlle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int ogrenciSayisi =12;
+         String mesaj = "Ögrenci sayısı :";
+         System.out.println(mesaj+ogrenciSayisi);
+         System.out.println(mesaj+ogrenciSayisi);
+         System.out.println("Ögrenci sayım: "+ogrenciSayisi);
+         System.out.println("Ögrenci sayım: "+ogrenciSayisi);
+         System.out.println("Ögrenci sayım: "+ogrenciSayisi);
+	}
+
+}
